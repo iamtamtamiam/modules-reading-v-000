784345369bc287c39831dba6fdcbb866e7f3bf1a
Tam Vu <vutam_a@hotmail.com>
2019-07-04 17:38:29 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -5,7 +5,7 @@ require_relative './fancy_dance.rb'
 
 class Kid 
   #include Dance 
-  e#xtend MetaDancing
+  #extend MetaDancing
   
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
